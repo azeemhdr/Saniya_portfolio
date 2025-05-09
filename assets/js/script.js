530ec97ea768bf970e9e6bf19bd0039e083edfd1
azeemhdr <comazeem@gmail.com>
2025-05-09 13:52:45 +0530

    aos added on home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// Header js===============================
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 200) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
+  });
+// HERO JS=======================
 document.addEventListener("DOMContentLoaded", function () {
     const typingText = "WITH SANIYA";
     document.querySelector(".typing").textContent = typingText;
